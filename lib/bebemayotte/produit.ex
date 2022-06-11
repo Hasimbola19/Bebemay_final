d1087d35371ca7fa1e0f2282940b1a449bbc6384
Hasimbola <Hasimbola@PHIDIA.local>
2022-06-11 13:32:32 +0300

    Ajout de produit.ex

--- lib/bebemayotte/produit.ex
+++ lib/bebemayotte/produit.ex
@@ -12,13 +12,14 @@ defmodule Bebemayotte.Produit do
     field :stockstatus, :boolean, default: false
     field :title, :string
     field :id_user, :integer
+    field :imageVersion, :integer
 
   end
 
   @doc false
   def changeset(produit, attrs) do
     produit
-    |> cast(attrs, [:id_produit, :title, :photolink, :id_cat, :id_souscat, :stockstatus, :stockmax, :price, :id_user])
+    |> cast(attrs, [:id_produit, :title, :photolink, :id_cat, :id_souscat, :stockstatus, :stockmax, :price, :id_user, :imageVersion])
     |> validate_required([:id_produit])
   end
 end
