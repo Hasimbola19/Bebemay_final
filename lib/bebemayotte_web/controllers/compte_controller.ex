c7dbfaad0c253c168b7450d8b1371e24f8231144
Hasimbola <Hasimbola@PHIDIA.local>
2022-06-13 15:47:35 +0300

    Ajout de controller

--- lib/bebemayotte_web/controllers/compte_controller.ex
+++ lib/bebemayotte_web/controllers/compte_controller.ex
@@ -331,9 +331,9 @@ defmodule BebemayotteWeb.CompteController do
       pbx_retour = "Mt:M;Ref:R;Auto:A;Erreur:E";
 
       # // Paramétrage des urls de redirection navigateur client après paiement :
-      pbx_effectue = "http://localhost:4001/accepte"
-      pbx_annule = "http://localhost:4001/annule"
-      pbx_refuse = "http://localhost:4001/refuse"
+      pbx_effectue = "http://162.19.74.21.fr:4000/accepte"
+      pbx_annule = "http://162.19.74.21.fr:4000/annule"
+      pbx_refuse = "http://162.19.74.21.fr:4000/refuse"
 
       # // On récupère la date au format ISO-8601 :
       {erl_date, erl_time} = :calendar.local_time()
