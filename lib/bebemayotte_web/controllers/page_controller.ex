78176ef67e4cdc5c75fa130dfc780379e256e378
Hasimbola <Hasimbola@PHIDIA.local>
2022-06-22 11:37:43 +0300

    Modifiation du controller pour le dropdown navbar

--- lib/bebemayotte_web/controllers/page_controller.ex
+++ lib/bebemayotte_web/controllers/page_controller.ex
@@ -18,7 +18,7 @@ defmodule BebemayotteWeb.PageController do
     categories = CatRequette.get_all_categorie()
     souscategories = SouscatRequette.get_all_souscategorie()
 
-    render(conn, "index.html", categories: categories,souscategories: souscategories, search: nil)
+    render(conn, "index.html", categories: categories, souscategories: souscategories, search: nil)
   end
 
   def location(conn, _params) do
