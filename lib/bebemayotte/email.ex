8310533a5fed4b79a0fae5ca9f292e7484ddef2a
Hasimbola <Hasimbola@PHIDIA.local>
2022-06-28 14:52:12 +0300

    Modification de la configuration smtp

--- lib/bebemayotte/email.ex
+++ lib/bebemayotte/email.ex
@@ -4,15 +4,14 @@ defmodule Bebemayotte.Email do
   # CREATE NEW MAIL MESSAGE
   def new_mail_message(email, message) do
     new_email()
-    |>from("noreply@bbmay.fr")
+    |>from("bbmaymayotte@outlook.fr")
     |>to(email)
     |>subject("Objet: VERIFICATION POUR MOT DE PASSE OUBLIÉ")
     |>html_body("<h1><b>BBMAY</b></h1><br/><p>Veuillez suivre le lien ci-dessous pour modifier votre mot de passe:</p><br/> #{message}")
   end
 
-  @spec send :: Bamboo.Email.t()
   def send do
-    new_mail_message("razafintsalama.rmh@gmail.com", "Nasolo a, tonga ve le mail") |> Mailer.deliver_now()
+    new_mail_message("rakotosonhasina00@gmail.com", "Nasolo a, tonga ve le mail") |> Mailer.deliver_now()
   end
 
 end
